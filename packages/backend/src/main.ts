ff8c0b83b938416cd814291659fbd7aa52c22fb3
Mohamed Elbashar <mohamedbashar200@gmail.com>
2023-11-11 20:09:15 +0200

    FIX : deploy

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(parseInt(process.env.PORT) || 3000, '0.0.0.0');
+  await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
